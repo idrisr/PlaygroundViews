0412111d426f6ec8ea6cfed33e5c03152552a36d
Id Raja <iraja@nerdery.com>
2016-12-12 11:49:57 -0600

    Initial Commit

---
+++ lightningTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lightningTalk
+//
+//  Created by Id Raja on 12/12/16.
+//  Copyright © 2016 Id Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
